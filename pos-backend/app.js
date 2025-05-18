04f3558a6c95fda8866035faf172071a99bdf5ee
Channa Maduranga <83961725+ChannaMaduranga@users.noreply.github.com>
2025-05-19 00:43:53 +0530

    set until register

---
+++ pos-backend/app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB=require("./config/database");
+const config = require("./config/config")
+const globalErrorHandler = require("./middlewares/globalErrorHandler");
+const createHttpError = require("http-errors");
+const app = express();
+
+const PORT = config.port;
+connectDB();
+
+// Middleware
+app.use(express.json()); // needed to parse req.body
+
+
+app.get("/",(req,res) =>{
+    res.json({message: "Hello from POS Server!"});
+})
+
+//end points
+app.use("/api/user",require("./routes/userRoute"));
+
+// Global Error Handler
+app.use(globalErrorHandler);
+
+app.listen(PORT,()=>{
+    console.log(`POS server is listening on port ${PORT}`);
+})
